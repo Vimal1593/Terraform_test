815321b4e58b3225f9fd044ed9346f701b92fe10
Vimal1593 <81192274+Vimal1593@users.noreply.github.com>
2023-07-03 22:57:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+  region = "us-west-2"
 
+  assume_role {
+    role_arn     = "arn:aws:iam::246718186480:role/terraformrl"
+    session_name = "AssumeRoleSession"
+    external_id  = "vimalrajk"  # Optional: If your role requires an external ID
+  }
+}
