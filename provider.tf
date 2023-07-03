9c22c7ffc5a35544af83afcccd8304b45f5e2451
Vimal1593 <81192274+Vimal1593@users.noreply.github.com>
2023-07-03 23:27:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
-  region = "us-west-2"
-
+  region = "us-west-1"
   assume_role {
     role_arn     = "arn:aws:iam::246718186480:role/terraformrl"
     session_name = "AssumeRoleSession"
